16fb3da6b82fb28e5d52c5db743dc419dd56d83a
John Odom <john.osborne.odom@gmail.com>
2019-09-11 14:27:36 -0400

    backend commit

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -33,6 +33,40 @@ class TeamsController < ApplicationController
         team.save
         render json: {name:team[:name], players:team.players}
     end
+
+    def update
+        team=Team.find(params["id"])
+        goalkeeper=Player.find(params["players"][0])
+        lb=Player.find(params["players"][1])
+        lcb=Player.find(params["players"][2])
+        rcb=Player.find(params["players"][3])
+        rb=Player.find(params["players"][4])
+        cdm=Player.find(params["players"][5])
+        lam=Player.find(params["players"][6])
+        ram=Player.find(params["players"][7])
+        lw=Player.find(params["players"][8])
+        st=Player.find(params["players"][9])
+        rw=Player.find(params["players"][10])
+        # team.name = params["teamName"]
+        lineups = Lineup.where(team_id: team.id)
+        lineups.each do |lineup|
+            lineup.destroy
+        end
+        team.players << st
+        team.players << lw
+        team.players << rw
+        team.players << lam
+        team.players << ram
+        team.players << cdm
+        team.players << lb
+        team.players << lcb
+        team.players << rcb
+        team.players << rb
+        team.players << goalkeeper
+        team.update(name: params["teamName"])
+        render json: {name:team[:name], players:team.players}
+    end
+
     def destroy
         team = Team.find(params[:id])
         team.destroy
